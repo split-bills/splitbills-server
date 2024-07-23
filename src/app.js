ec1c340be088bce89ae31d1990d5bc3c085e2a79
Siddesh Devarakonda <siddeshdslr@gmail.com>
2024-07-23 16:57:50 +0530

    add hasing, salting for auth

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ require("dotenv").config();
 const homeRoutes = require("./routes/homeRoutes");
 const eventsRoutes = require("./routes/eventsRoutes");
 const profileRoutes = require("./routes/profileRoutes");
+const authRoutes = require("./routes/authRoutes");
 // const statsRoutes = require('./routes/statsRoutes');
 
 const app = express();
@@ -56,62 +57,6 @@ function isAuthenticated(req, res, next) {
 app.use("/home", isAuthenticated, homeRoutes); // Protecting the home route
 app.use("/events", isAuthenticated, eventsRoutes); // Protecting the events route
 app.use("/profile", isAuthenticated, profileRoutes); // Protecting the profile route
-
-app.post("/signup", async (req, res) => {
-  console.log("SignUp Started...");
-  try {
-    const signUpDetails = req.body;
-    console.log("Signup Details: ", signUpDetails);
-    const client = await pool.connect();
-    const result = await client.query(
-      `INSERT INTO users (first_name, last_name, email, hashed_password)
-      VALUES ($1, $2, $3, $4) RETURNING user_id`,
-      [
-        signUpDetails.firstName,
-        signUpDetails.lastName,
-        signUpDetails.email,
-        signUpDetails.password,
-      ]
-    );
-    client.release();
-    res.status(201).json({ message: "User created successfully" });
-  } catch (error) {
-    console.error("Error retrieving data from database", error);
-    res.status(500).json({ message: "Internal server error" });
-  } finally {
-    console.log("SignUp completed..");
-  }
-});
-
-app.post("/login", async (req, res) => {
-  console.log("Login started...");
-  try {
-    const loginDetails = req.body;
-    console.log("Login details: ", loginDetails);
-    const client = await pool.connect();
-    const result = await client.query(
-      "SELECT id, hashed_password, email FROM users WHERE email = $1",
-      [loginDetails.email]
-    );
-
-    if (
-      result.rows.length > 0 &&
-      result.rows[0].hashed_password === loginDetails.password
-    ) {
-      req.session.userId = result.rows[0].id; // Store user ID in session
-      console.log("Authenticated!!!");
-      res.json({ authenticated: true });
-    } else {
-      console.log("Authentication failed!!!");
-      res.json({ authenticated: false });
-    }
-    client.release();
-  } catch (error) {
-    console.error("Error retrieving data from database", error);
-    res.status(500).json({ message: "Internal server error" });
-  } finally {
-    console.log("Login completed..");
-  }
-});
+app.use("/auth", authRoutes);
 
 module.exports = app;
---
+++ src/controllers/authController.js
@@ -0,0 +1,96 @@
+const pool = require("../config/db").pool;
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
+
+// All post requests
+exports.signUp = async (req, res) => {
+  console.log("SignUp Started...");
+  const signUpDetails = req.body;
+  console.log("Signup Details: ", signUpDetails);
+
+  try {
+    const client = await pool.connect();
+
+    const checkResult = await client.query(
+      "SELECT id FROM users WHERE email = $1",
+      [signUpDetails.email]
+    );
+
+    if (checkResult.rows.length > 0) {
+      res.status(401).json({ message: "User already exists" });
+    }
+
+    // Password hashing
+    bcrypt.hash(signUpDetails.password, saltRounds, async (err, hash) => {
+      if (err) {
+        console.error("Error hashing password: ", err);
+        res.status(500).json({ message: "Internal server error" });
+      }
+      const result = await client.query(
+        `INSERT INTO users (first_name, last_name, email, hashed_password)
+      VALUES ($1, $2, $3, $4)`,
+        [
+          signUpDetails.firstName,
+          signUpDetails.lastName,
+          signUpDetails.email,
+          hash,
+        ]
+      );
+    });
+
+    client.release();
+
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    console.error("Error retrieving data from database", error);
+    res.status(500).json({ message: "Internal server error" });
+  } finally {
+    console.log("SignUp completed..");
+  }
+};
+
+exports.login = async (req, res) => {
+  console.log("Login started...");
+  const loginDetails = req.body;
+  console.log("Login details: ", loginDetails);
+
+  try {
+    const client = await pool.connect();
+
+    const result = await client.query(
+      "SELECT id, hashed_password, email FROM users WHERE email = $1",
+      [loginDetails.email]
+    );
+
+    if (result.rows.length > 0) {
+      const user = result.rows[0];
+      const storedHashedPassword = user.hashed_password;
+      bcrypt.compare(
+        loginDetails.password,
+        storedHashedPassword,
+        (err, result) => {
+          if (err) {
+            console.error("Error comparing passwords: ", err);
+          } else {
+            if (result) {
+              req.session.userId = user.id; // Store user ID in session
+              console.log("Authenticated!!!");
+              res.json({ authenticated: true });
+            } else {
+              console.log("Authentication failed!!!");
+              res.json({ authenticated: false });
+            }
+          }
+        }
+      );
+    }
+
+    client.release();
+  } catch (error) {
+    console.error("Error retrieving data from database", error);
+    res.status(500).json({ message: "Internal server error" });
+  } finally {
+    console.log("Login completed..");
+  }
+};

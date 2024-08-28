bf599bc4e9ac3d662022ed1a4d850e65d0006222
Siddesh Devarakonda <siddeshdslr@gmail.com>
2024-08-28 11:25:25 +0530

    vercel

--- src/config/db.js
+++ src/config/db.js
@@ -1,20 +1,24 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+const { Pool } = require("pg");
+require("dotenv").config();
+
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.POSTGRES_URL,
 });
 
 const connectDB = async () => {
   try {
     await pool.connect();
-    console.log('PostgreSQL connected');
+    console.log("PostgreSQL connected");
   } catch (error) {
-    console.error('PostgreSQL connection failed', error);
+    console.error("PostgreSQL connection failed", error);
     process.exit(1);
   }
 };
